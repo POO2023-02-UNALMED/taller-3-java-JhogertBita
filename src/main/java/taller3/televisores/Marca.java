777b8b54fe8fa4512d24acffb812b5f4bc6b7ace
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-08-29 09:18:16 -0500

    Código completado

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,22 @@
+public package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	public Marca(String nombre) {
+		this.nombre=nombre;
+	}
+	public String getMarca() {
+		return nombre;
+	}
+	public void setMarca(String z) {
+		this.nombre=z;
+	}
+	public void setNombre(String z) {
+		this.nombre=z;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+} Marca {
+    
+}
