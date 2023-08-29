3054045ca7f55c3e41c1b1376b6d2d48323152d8
JhogertBita <141960659+JhogertBita@users.noreply.github.com>
2023-08-29 09:18:12 -0500

    Código completado

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,86 @@
+package taller3.televisores;
+
+public class TV {
+	private Marca marca;
+	int canal=1;
+	private int precio=500;
+	boolean estado;
+	int volumen=1;
+	private Control control;
+	public static int numTV;
+	TV tv;
+	
+	public TV(Marca marc,boolean estado) {
+		this.marca=marc;
+		this.estado=estado;
+		numTV ++;
+	}
+		public Marca setMarca(Marca z) {
+			return this.marca=z;
+	}
+		public Marca getMarca() {
+			return marca;
+		}
+		public void setCanal(int canal) {
+			if (estado==true & canal>=1 & canal<=120) {
+			this.canal=canal;
+			}
+			}
+		public int getCanal() {
+			return canal;
+		}
+		public int setPrecio(int precio) {
+			return this.precio=precio;
+	}
+		public int getPrecio() {
+			return precio;
+		}
+		public void setVolumen(int volumen) {
+			if (estado==true & volumen<=7 & volumen>=0) {
+			this.volumen=volumen;}
+	}
+		public int getVolumen() {
+			return volumen;
+		}
+		public Control setControl(Control control) {
+			return this.control=control;
+	}
+		public Control getControl() {
+			return control;
+		}
+	public boolean getEstado() {
+		return estado;
+	}
+	public boolean turnOn() {
+		return this.estado=true;
+	}
+	public boolean turnOff() {
+		return this.estado=false;
+	}
+	public void canalUp() {
+		if(estado==true & canal<120) {
+			canal++;
+		}
+	}
+	public void canalDown() {
+		if(estado==true & canal>1) {
+		canal--;}
+	}
+	public void volumenUp() {
+		if(estado==true & volumen<7){
+		volumen++;}
+	}
+	public void volumenDown() {
+		if(estado==true & volumen>0) {
+		volumen--;}
+	}
+	public Object getTv() {
+		return tv;
+	}
+	public static int getNumTV() {
+		return numTV;
+	}
+	public static int setNumTV(int numtv) {
+		return numTV=numtv;
+	}
+}
